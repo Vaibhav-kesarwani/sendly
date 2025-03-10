b1b6fbc6f413619541a0d43a623aba1e3e525d5b
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-03-10 19:36:08 +0530

    Chnage the border theme ans style

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 h-20 w-full bg-zinc-800 px-6 text-white">
+    <footer className="fixed bottom-0 left-0 h-10 w-full border-t border-white/40 bg-transparent px-6 text-zinc-800 shadow-lg backdrop-blur-lg">
       <div className="mx-auto flex h-full w-full max-w-screen-xl items-center justify-between">
-        <h1 className="text-sm text-zinc-300">
+        <h1 className="text-sm">
           Created by{" "}
           <Link
-            href={"https://www.linkedin.com/in/vaibhav-kesarwani-9b5b35252/"}
+            href="https://www.linkedin.com/in/vaibhav-kesarwani-9b5b35252/"
             target="_blank"
-            className="font-medium"
+            className="font-medium text-blue-600 transition hover:text-blue-800"
           >
             Vaibhav Kesarwani
           </Link>
@@ -19,7 +19,8 @@ export default function Footer() {
         <div>
           <Link
             target="_blank"
-            href={"https://github.com/Vaibhav-kesarwani"}
+            href="https://github.com/Vaibhav-kesarwani"
+            className="text-zinc-700 transition hover:text-black"
           >
             <Github />
           </Link>
